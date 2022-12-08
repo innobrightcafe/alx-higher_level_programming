d04aac9c15eb842a74e4f40d160674fc25bfc49a
innobrightcafe <innobrightcafe@gmail.co>
2022-12-08 09:50:09 +0100

    square matrix

--- 0x04-python-more_data_structure/0-square_matrix_simple.py
+++ 0x04-python-more_data_structure/0-square_matrix_simple.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+
+    squared_matrix = []
+
+    for row in matrix:
+
+        squared_row = []
+
+        for value in row:
+
+            squared_row.append(value ** 2)
+
+        squared_matrix.append(squared_row)
+
+    return squared_matrix
+
 
