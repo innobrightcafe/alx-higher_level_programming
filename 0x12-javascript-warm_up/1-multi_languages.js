d343f75006ff170cddd3a2ac61b058ab8b765219
macbook <macbook@inosoft-MacBook-Pro.local>
2023-03-15 20:39:44 +0100

    javacript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar = 'JavaScript is amazing';
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar);
