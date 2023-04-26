937720be6fcda916b83ca5171a7fedab750eeacf
inosoft <innobrightcafe@gmail.com>
2023-04-26 10:31:59 -0400

    craping the web with javascript

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, contents) {
-  if (contents === undefined) {
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
     console.log(err);
   } else {
-    console.log(contents);
+    console.log(data);
   }
-});
+})
